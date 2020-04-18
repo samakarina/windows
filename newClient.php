855a72f50369b7d2af926c1bb67601b9286e2f5b
nixelce <31865595+nixelce@users.noreply.github.com>
2020-04-18 15:32:13 +0300

    запись обращения в БД
    Добавлена форма HTML (при вводе телефона используется маска), вводимые значения помещаются в БД

--- newClient.php
+++ newClient.php
@@ -1,5 +1,32 @@
 <?php
 
-echo "123";
+$link = mysqli_connect(
+    'localhost',
+    'mysql',
+    'mysql',
+    'contacts');
 
- ?>
+    if (!$link) {
+        printf("Невозможно подключиться к базе данных. Код ошибки: %s\n", mysqli_connect_error());
+        exit;
+    }
+
+    $newClient = $_POST;
+    var_dump($newClient);
+    $sql = "INSERT INTO clients (nameClient, phone, comment) VALUES ('".$newClient["nameClient"]."','".$newClient["phoneClient"]."','".$newClient["comment"]."')";
+    var_dump($sql);
+    if (mysqli_query($link, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($link);
+    }
+
+    if ($result = mysqli_query($link, 'SELECT * FROM Clients')) {
+        print("Клиенты:\n");
+        while( $row = mysqli_fetch_assoc($result) ){
+            printf("%s (%s)\n", $row['nameClient'], $row['phone']);
+        }
+        mysqli_free_result($result);
+    }
+    mysqli_close($link);
+    ?>
